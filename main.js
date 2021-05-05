21784d96ae05aecb9a8c2f12f7da9867ea4a2a2d
emiirnahuel <emiirnahuel@gmail.com>
2021-05-05 16:13:25 -0300

    add popup menu
    agrego el menu email popup y reparo errores

--- main.js
+++ main.js
@@ -2,6 +2,22 @@ document.querySelector('.menu-btn').addEventListener('click',()=> {
     document.querySelector('.nav-menu').classList.toggle('show');
 });
 
+var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
+
 ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards', { delay: 500});
 ScrollReveal().reveal('.cards-banner-one', { delay: 500});
